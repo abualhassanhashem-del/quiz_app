bb2c5239cc4977c0f36fc8432d99c3560786c6bb
abualhassanhashem-del <abu.alhassan.hashem@gmail.com>
2026-06-19 21:29:49 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 
-void main() {
-runApp(
-const MaterialApp(
-home: Scaffold(
-body: Center(
-child: Text('TEST'),
-),
-),
-),
-);
+import 'app/app.dart';
+import 'features/ads/ad_service.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  try {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+  } catch (e) {
+    debugPrint("Firebase Error: $e");
+  }
+
+  try {
+    await MobileAds.instance.initialize();
+  } catch (e) {
+    debugPrint("AdMob Error: $e");
+  }
+
+  AdService.loadInterstitialAd();
+  AdService.loadRewardedAd();
+
+  runApp(const GeniusChallengeApp());
 }
